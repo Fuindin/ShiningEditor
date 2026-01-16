a980b1b4249b40388f65411bbd4abd8df8373c04
Jack Reed <jackreed30@yahoo.com>
2026-01-16 12:54:16 -0700

    Add files via upload
    Some updates to the About form.

--- ShiningEditor/AboutForm.Designer.cs
+++ ShiningEditor/AboutForm.Designer.cs
@@ -28,39 +28,11 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AboutForm));
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
+            this.descriptionRichTb = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(16, 101);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(282, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Shining Editor is a Genesis / Mega Drive save state editor.";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(16, 118);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(345, 13);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "It allows a user to edit the party\'s gold, experience, and various stats for ";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(16, 135);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(250, 13);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "each playable character in Shining in the Darkness.";
             // 
             // label4
             // 
@@ -80,16 +52,23 @@
             this.label5.TabIndex = 4;
             this.label5.Text = "Programmed by: Fuindin";
             // 
+            // descriptionRichTb
+            // 
+            this.descriptionRichTb.Location = new System.Drawing.Point(19, 83);
+            this.descriptionRichTb.Name = "descriptionRichTb";
+            this.descriptionRichTb.ReadOnly = true;
+            this.descriptionRichTb.Size = new System.Drawing.Size(342, 192);
+            this.descriptionRichTb.TabIndex = 5;
+            this.descriptionRichTb.Text = resources.GetString("descriptionRichTb.Text");
+            // 
             // AboutForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(414, 339);
+            this.Controls.Add(this.descriptionRichTb);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
             this.Name = "AboutForm";
             this.Text = "About Shining Editor";
             this.ResumeLayout(false);
@@ -98,11 +77,8 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.RichTextBox descriptionRichTb;
     }
 }
